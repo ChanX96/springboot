1173fe4ae6cbee0f64bb27ff50dd8a6c42d0bc1f
ChanX96 <821602490@qq.com>
2020-01-23 14:09:50 +0800

    Initial commit

---
+++ p01_helloworld/src/test/java/com/chanx/p01_helloworld/P01HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanx.p01_helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P01HelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
